3b6622fda413922873213d18a328d6c67b5c489c
徐德宇 <tai_365@qq.com>
2025-12-14 21:41:49 +0800

    Add calculator and break the test

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        // 这里是正确的逻辑：1+1=2
+        // 等会儿做第七步时，你要把这里的 2 改成 5
+        Assert.assertEquals(5, calculator.add(1, 1));
+    }
+}
